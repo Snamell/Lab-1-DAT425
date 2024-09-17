64bf3313e009f86e2720870196eb5502a51fca18
Snamell <118366491+Snamell@users.noreply.github.com>
2024-09-17 10:49:01 +0200

    test

---
+++ wordfreq.py
@@ -0,0 +1,11 @@
+def tokenize(lines):
+    words=[]
+    for line in lines:
+        start = 0
+    while start < len(line):
+        print(line[start])
+        start = start+1
+    return words
+
+
+

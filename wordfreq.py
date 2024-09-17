8ce48e13008f8eba81a6b11353be1f4578c9e8b2
Snamell <118366491+Snamell@users.noreply.github.com>
2024-09-17 16:23:13 +0200

    early beta 0.01

--- wordfreq.py
+++ wordfreq.py
@@ -2,10 +2,17 @@ def tokenize(lines):
     words=[]
     for line in lines:
         start = 0
-    while start < len(line):
-        print(line[start])
-        start = start+1
+        while start < len(line):
+            print(line[start].lower(), end="")
+            start = start+1
     return words
 
-
-
+def countWords(a,b):
+    pass
+    
+def printTopMost(x,y):
+    pass
+    
+fil=open("articles/article1.txt")
+tokenize(fil)
+fil.close()

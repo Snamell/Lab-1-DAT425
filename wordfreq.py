341121f6d0b3fd7c49c703771b15de61c1ea92fb
Snamell <118366491+Snamell@users.noreply.github.com>
2024-09-24 12:24:24 +0200

    tokenize function created

--- wordfreq.py
+++ wordfreq.py
@@ -1,18 +1,38 @@
 def tokenize(lines):
-    words=[]
+    words = []
+
     for line in lines:
         start = 0
-        while start < len(line):
-            print(line[start].lower(), end="")
-            start = start+1
-    return words
 
-def countWords(a,b):
-    pass
-    
-def printTopMost(x,y):
-    pass
-    
-fil=open("articles/article1.txt")
-tokenize(fil)
-fil.close()
+        while start < len(line):
+
+            if line[start].isspace():
+                start=start+1
+
+            elif line[start].isalpha():
+                end=start 
+                try:
+                    while line[end].isalpha():
+                        end=end+1
+                    print(line[start:end])
+                    words.append(line[start:end].lower())
+                    start=end
+                except IndexError:
+                    print(line[start:end])
+                    words.append(line[start:end].lower())
+                    return words
+
+            elif line[start].isdigit():
+                end=start
+                while line[end].isdigit():
+                    end=end+1
+                print(line[start:end])
+                words.append(line[start:end])
+                start=end
+
+            else:
+                print(line[start])
+                words.append(line[start])
+                start=start+1
+
+    return words
